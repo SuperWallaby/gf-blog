c5b5cb94b90a50f9ec5913962cd1f399f432a49f
SuperWallaby <colton950901@gmail.com>
2020-09-22 23:21:11 +0900

    .

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -166,7 +166,7 @@ AWS_STORAGE_BUCKET_NAME = 'dobalove'
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_ROOT = "staticfiles/"
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
